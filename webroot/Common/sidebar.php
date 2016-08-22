417f3befb6087fc152c1c24f97ad120eb64ecc92
Anwar <justdoit2045@gmail.com>
2016-08-22 10:35:12 +0545

    change

--- webroot/Common/sidebar.php
+++ webroot/Common/sidebar.php
@@ -1,9 +1,48 @@
 <?php
 use Cake\ORM\TableRegistry;
 ?>
+<?php
+          $cond_p = '';
+          $cond_t = '';
+          if((isset($pcat) && count($pcat)) || (isset($tcat) && count($tcat)))
+          {
+            if(isset($pcat) && count($pcat))
+            {
+                foreach($pcat as $pc)
+                {
+                    if($cond_p == '')
+                    $cond_p = 'id = '.$pc;
+                    else
+                    $cond_p = $cond_p.' OR '. 'id = '.$pc;
+                }
+            }
+            
+            if(isset($tcat) && count($tcat))
+            {
+                foreach($tcat as $tc)
+                {
+                    if($cond_t == '')
+                    $cond_t = 'id = '.$tc;
+                    else
+                    $cond_t = $cond_t.' OR '. 'id = '.$tc;
+                }
+            }
+          }
+          if($cond_p || $cond_t)
+          {
+            //$sql = "SELECT video_id FROM tags WHERE ".$cond;
+            if($cond_p)
+            $pack_side = TableRegistry::get('Packages')->find()->where([$cond_p])->order('rand()')->limit(4)->all();
+            if($cond_t)
+            $tour_side = TableRegistry::get('Tours')->find()->where([$cond_t])->order('rand()')->limit(4)->all();
+          }
+          
+          ?>
 <div class="sidebar col-md-3 col-sm-4">
             
               <?php 
+              if(!isset($pack_side) && !isset($tour_side))
+              {
               $pgcats = TableRegistry::get('PageCategory')->find()->order(['id'=>'desc'])->all();
               foreach($pgcats as $pgc)
               {
@@ -22,6 +61,58 @@ use Cake\ORM\TableRegistry;
                </ul>
                <hr />
                <?php 
+              }
+              }
+              else
+              {
+                if(isset($pack_side))
+                {
+                    ?>
+                    <h2 class="related">Related Packages</h2>
+                    <?php
+                    foreach($pack_side as $ps)
+                    {
+                        ?>
+                        <div class="side_group">
+                         <div class="image_side" style="height: 175px;position:relative;overflow:hidden;">
+                        <img style="max-width: 100%;position: absolute;top:-44px;" src="
+                        <?php 
+                        echo $this->request->webroot.'img/package/final/'.$ps->image;
+                        ?>
+                        " />
+                        
+                        </div>
+                        <a href="<?php echo $this->request->webroot?>package/<?php $ps->slug;?>"><?php echo $ps->title;?></a>
+                        </div>
+                        <?php
+                    }
+                    ?>
+                    <?php
+                }
+                if(isset($tour_side))
+                {
+                    ?>
+                    <h2 class="related">Related Tours</h2>
+                    <?php
+                    foreach($tour_side as $ps)
+                    {
+                        ?>
+                        <div class="side_group">
+                        <div class="image_side" style="height: 130px;position:relative;overflow:hidden;">
+                         <img style="max-width: 100%;position: absolute;top:-44px;" src="
+                        <?php 
+                        echo $this->request->webroot.'img/package/tour/'.$ps->image;
+                        ?>
+                        " />
+                        </div>
+                       <a href="<?php echo $this->request->webroot?>package/<?php $ps->slug;?>"><?php echo $ps->title;?></a>
+                        </div>
+                        <hr />
+                        <?php
+                    }
+                    ?>
+                    <?php
+                }
               }
               ?> 
               <?php
