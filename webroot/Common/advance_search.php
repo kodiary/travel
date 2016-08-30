d1a183a6053828493b3c619e0719eb21e78270a8
Anwar <justdoit2045@gmail.com>
2016-08-30 12:51:41 +0545

    changes

--- webroot/Common/advance_search.php
+++ webroot/Common/advance_search.php
@@ -3,11 +3,23 @@ if(isset($key))
 $_GET['keyword'] = $key;
 else
 $_GET['keyword'] = ' ';
+
+if(isset($_GET['country']) && $_GET['country'])
+{
+    $coun = $_GET['country'];
+}
+else
+$coun = '';
+
+if(isset($_GET['days']))
+{
+    $days = $_GET['days'];
+}
 ?>
      <h2 class="related">Advance Search</h2>
      <form method="get" action="<?php echo $this->request->webroot;?>search" style="background: #FFF;padding:10px;">
          <div class="form-group">
-         <label class="col-md-3 padding-left-0"><strong>Keyword</strong></label><div class="col-md-9"><input type="text" class="form-control" name="keyword" value="<?php echo $_GET['keyword'];?>" style="height: 23px;padding: 0 5px" /></div>
+         <div class="col-md-12 padding-left-0 padding-right-0"><input type="text" placeholder="Keyword" class="form-control" name="keyword" value="<?php echo $_GET['keyword'];?>" style="height: 27px;padding: 0 5px" /></div>
          <div class="clearfix"></div>
          </div>   
          <div class="form-group">
@@ -18,20 +30,20 @@ $_GET['keyword'] = ' ';
                  foreach($country as $co)
                  {
                     ?>
-                    <option value="<?php echo $co->id;?>"><?php echo $co->countryName;?></option>
+                    <option value="<?php echo $co->id;?>" <?php if($coun == $co->id){?>selected="selected"<?php }?>><?php echo $co->countryName;?></option>
                     <?php
                  }
                  ?>
              </select>
          </div>
          <div class="form-group">
-             <label class="col-md-2 padding-left-0"><strong>Type</strong></label>
-             <div class="col-md-10">
-                <input type="checkbox" name="Packages" value="1" onchange="if($(this).is(':checked'))$('.p_hide').show();else{$('.p_hide').hide();}" /> Packages &nbsp; &nbsp; <input type="checkbox" name="Tours" value="1" onchange="if($(this).is(':checked'))$('.t_hide').show();else{$('.t_hide').hide();}" /> Tours
+             
+             <div class="col-md-12 padding-left-0">
+                <input type="checkbox" name="Packages" <?php if(isset($_GET['Packages']) && $_GET['Packages']){?>checked="checked"<?php }?> value="1" onchange="if($(this).is(':checked'))$('.p_hide').show();else{$('.p_hide').hide();}" /> Packages &nbsp; &nbsp; <input type="checkbox" name="Tours" <?php if(isset($_GET['Tours']) && $_GET['Tours']){?>checked="checked"<?php }?> value="1" onchange="if($(this).is(':checked'))$('.t_hide').show();else{$('.t_hide').hide();}" /> Tours
              </div>
              <div class="clearfix"></div>
          </div>
-         <div class="form-group p_hide" style="display: none;">
+         <div class="form-group p_hide" style="<?php if(isset($_GET['Packages']) && $_GET['Packages']){?><?php }else{?>display: none;<?php }?>">
             <select name="pcat" class="form-control">
                  <option value="">PACKAGE CATEGORY</option>
                  <?php 
@@ -39,13 +51,13 @@ $_GET['keyword'] = ' ';
                  foreach($pcats as $co)
                  {
                     ?>
-                    <option value="<?php echo $co->id;?>"><?php echo $co->title;?></option>
+                    <option value="<?php echo $co->id;?>" <?php if(isset($_GET['pcat']) && $_GET['pcat']==$co->id){?>selected="selected"<?php }?>><?php echo $co->title;?></option>
                     <?php
                  }
                  ?>
              </select>
          </div>
-         <div class="form-group t_hide" style="display: none;">
+         <div class="form-group t_hide" style="<?php if(isset($_GET['Tours']) && $_GET['Tours']){?><?php }else{?>display: none;<?php }?>">
             <select name="tcat" class="form-control">
                  <option value="">TOUR CATEGORY</option>
                  <?php 
@@ -53,7 +65,7 @@ $_GET['keyword'] = ' ';
                  foreach($tcats as $co)
                  {
                     ?>
-                    <option value="<?php echo $co->id;?>"><?php echo $co->title;?></option>
+                    <option value="<?php echo $co->id;?>" <?php if(isset($_GET['pcat']) && $_GET['pcat']==$co->id){?>selected="selected"<?php }?>><?php echo $co->title;?></option>
                     <?php
                  }
                  ?>
@@ -62,10 +74,10 @@ $_GET['keyword'] = ' ';
          <div class="form-group" style="">
             <select name="days" class="form-control">
                  <option value="">PACKAGE/TOUR DAYS</option>
-                 <option value="7">1 - 7 days</option>
-                 <option value="14">8 - 14 days</option>
-                 <option value="21">15 - 21 days</option>
-                 <option value="100">21 days +</option>                 
+                 <option value="7" <?php if($days=='7'){?>selected="selected"<?php }?>>1 - 7 days</option>
+                 <option value="14" <?php if($days=='14'){?>selected="selected"<?php }?>>8 - 14 days</option>
+                 <option value="21" <?php if($days=='21'){?>selected="selected"<?php }?>>15 - 21 days</option>
+                 <option value="100" <?php if($days=='100'){?>selected="selected"<?php }?>>21 days +</option>                 
              </select>
          </div>
          <div class="form-group">
